9d52ed1e8af88fcc620d3ff92cef94c169d36fac
Georg Wiese <georgwiese@gmail.com>
2023-08-23 09:08:37 +0000

    Undo change, fix test

--- executor/src/witgen/machines/block_machine.rs
+++ executor/src/witgen/machines/block_machine.rs
@@ -318,9 +318,7 @@ impl<'a, T: FieldElement> BlockMachine<'a, T> {
 
             let result = identity_processor.process_link(left, right, &row_pair)?;
 
-            // If there are any updates, they have to be in the current machine, which means we'll
-            // actually want to run it.
-            if result.is_complete() && result.constraints.is_empty() {
+            if result.is_complete() {
                 return Ok(result);
             }
         }
